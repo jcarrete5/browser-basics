560eadfb1e7f00e59a979c59358ee9d640cc14da
Jason R. Carrete <jcarrete5@users.noreply.github.com>
2019-05-04 17:57:55 -0400

    Remove transpiled js from vcs

--- extension/background.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.tabs.create({
-        active: true,
-        url: "https://google.com/"
-    }, function () {
-    });
-});
