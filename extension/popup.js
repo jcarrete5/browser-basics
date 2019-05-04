4247c42296590164b0dc736f0702d5b7b604fe21
Jason R. Carrete <jcarrete5@users.noreply.github.com>
2019-05-04 17:53:36 -0400

    Add TODO for when browser link is clicked

--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,7 @@
 "use strict";
 document.addEventListener('DOMContentLoaded', function () {
     var learn = document.getElementById('learn');
+    var browser = document.getElementById('browser');
     if (learn != null) {
         learn.addEventListener('click', function () {
             chrome.tabs.create({ url: chrome.runtime.getURL('learn_modules/index.html') });
@@ -8,5 +9,13 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     else {
         console.error("`learn` id is null");
+    }
+    if (browser != null) {
+        browser.addEventListener('click', function () {
+            //TODO what happens when they want to replay the browser tutorial
+        });
+    }
+    else {
+        console.log("`browser` id is null");
     }
 });
--- src/popup.ts
+++ src/popup.ts
@@ -1,10 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   let learn = document.getElementById('learn')
+  let browser = document.getElementById('browser');
   if (learn != null) {
     learn.addEventListener('click', () => {
       chrome.tabs.create({url: chrome.runtime.getURL('learn_modules/index.html')});
     });
   } else {
     console.error("`learn` id is null");
+  }
+  if (browser != null) {
+    browser.addEventListener('click', () => {
+      //TODO what happens when they want to replay the browser tutorial
+    });
+  } else {
+    console.log("`browser` id is null");
   }
 });
