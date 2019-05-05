b28c9da6c35e24ddd17874c928439066599868aa
Jason R. Carrete <jcarrete5@users.noreply.github.com>
2019-05-04 21:57:09 -0400

    Remove alerts from checks.ts

--- src/checks.ts
+++ src/checks.ts
@@ -2,7 +2,12 @@ let protocol = window.location.protocol;
 protocol = protocol.substring(0, protocol.length - 1);
 let hasPassword = document.querySelector('input[type="password"]') != null;
 if (protocol === 'http' && hasPassword) {
-    alert("Don't enter your password");
+    // alert("Don't enter your password");
+    chrome.runtime.sendMessage({
+        action: 'CreateDialog',
+        type: 'text',
+        content: "This site isn't secure. Don't enter any sensitive information"
+    });
 } else {
-    alert("Safe");
+    console.log("This site is safe");
 }
