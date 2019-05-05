5f479277db26b8b6c63718ffbac6132b092904ff
Zach <zachmatuson@gmail.com>
2019-05-04 22:31:23 -0400

    Added dialog content script

--- src/background.ts
+++ src/background.ts
@@ -6,5 +6,18 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.runtime.onMessage.addListener(async (message, sender) => {
+	chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+		const tabId = tabs[0].id;
+		if (!tabId) {
+			return;
+		}
+		switch (message.target) {
+			case "dialog":
+				chrome.tabs.sendMessage(tabId, message.content);
+				break;
 
+			default:
+				break;
+		}
+	});
 });
